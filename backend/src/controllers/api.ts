a28627a35093ee8b24b27ebd5da298126e275da7
Stefan Zurfluh <stefan.zurfluh@zuehlke.com>
2017-10-06 23:30:12 +0800

    movie API proxy & clean up
    - redirect each request to The Movie DB. The URL path+query are appended to the movie base url
    - removed unneeded packages and files

--- backend/src/controllers/api.ts
+++ backend/src/controllers/api.ts
@@ -1,33 +1,22 @@
 "use strict";
 
+import { URL } from "url";
 import * as async from "async";
 import * as request from "request";
-import * as graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
 
+const THE_MOVIE_DB_API_BASE = "https://api.themoviedb.org/3";
+const THE_MOVIE_DB_API_KEY = "00e139cee8bd741b03785ab5b22aca5c";
 
 /**
  * GET /api
  * List of API examples.
  */
 export let getApi = (req: Request, res: Response) => {
-  res.render("api/index", {
-    title: "API Examples"
-  });
-};
+  const movieUrl = new URL(`${THE_MOVIE_DB_API_BASE}${req.originalUrl}`);
+  movieUrl.searchParams.append("api_key", THE_MOVIE_DB_API_KEY);
 
-/**
- * GET /api/facebook
- * Facebook API example.
- */
-export let getFacebook = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.user.tokens.find((token: any) => token.kind === "facebook");
-  graph.setAccessToken(token.accessToken);
-  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-    if (err) { return next(err); }
-    res.render("api/facebook", {
-      title: "Facebook API",
-      profile: results
-    });
-  });
+  console.log(`Requesting ${movieUrl.href}...`);
+  const apiRequest = request(movieUrl.href);
+  apiRequest.pipe(res);
 };
--- backend/src/controllers/home.ts
+++ backend/src/controllers/home.ts
@@ -5,7 +5,5 @@ import { Request, Response } from "express";
  * Home page.
  */
 export let index = (req: Request, res: Response) => {
-  res.render("home", {
-    title: "Home"
-  });
+  res.send("home");
 };

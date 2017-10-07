a64d10612e1751b0bb685258627a5cb1a9ca79f7
Florian Besser <flbe@zuehlke.com>
2017-10-07 13:29:25 +0200

    Now also prefetch next page, do not just cache previouis results

--- backend/src/controllers/api.ts
+++ backend/src/controllers/api.ts
@@ -7,6 +7,36 @@ import * as request from "request";
 import * as NodeCache from "node-cache";
 import { Response, Request, NextFunction } from "express";
 
+interface DiscoverTvQuery {
+  language?: string;
+  sort_by?: string;
+  page?: number;
+  // ...
+}
+
+interface TvShowResult {
+  page: number;
+  total_results: number;
+  total_pages: number;
+  results: TvShow[];
+}
+
+interface TvShow {
+  poster_path: string;
+  populatiry: number;
+  id: number;
+  backdrop_path: string | null;
+  vote_average: number;
+  overview: string;
+  first_air_date: string;
+  origin_country: string[];
+  genre_ids: number[];
+  original_language: string;
+  vote_count: number;
+  name: string;
+  original_name: string;
+}
+
 const THE_MOVIE_DB_API_BASE = "https://api.themoviedb.org/3";
 const THE_MOVIE_DB_API_KEY = "00e139cee8bd741b03785ab5b22aca5c";
 
@@ -20,7 +50,7 @@ export let getApi = (req: Request, res: Response) => {
   const movieUrl = url.parse(`${THE_MOVIE_DB_API_BASE}${req.originalUrl}`);
 
   console.log(`Checking ${movieUrl.href} in cache`);
-  const value = myCache.get<Buffer>( movieUrl.href );
+  const value = myCache.get<string>( movieUrl.href );
 
   if ( value == undefined ) {
     console.log(`Didn't find ${movieUrl.href} in cache`);
@@ -31,23 +61,57 @@ export let getApi = (req: Request, res: Response) => {
 
     console.log(`Requesting ${movieUrlWithKey}`);
 
-    const apiRequest = request(movieUrlWithKey);
-
-    const bufs: Buffer[] = [];
-    apiRequest.on("data", function(d: Buffer){
-      bufs.push(d);
-    });
-    apiRequest.on("end", function(){
-      const buf = Buffer.concat(bufs);
-      myCache.set<Buffer>( movieUrl.href, buf );
+    const apiRequest = request(movieUrlWithKey, undefined, function (error, response: request.RequestResponse, body: string) {
+      handleApiResponse(movieUrl, error, response, body);
     });
 
     apiRequest.pipe(res);
   } else {
-    console.log(`Found ${movieUrl.href} in cache, length: ${value.length}`);
+    console.log(`Found ${movieUrl.href} in cache, ${value.length}`);
+
+    parseResultAndQueryNext(value);
 
     res.write(value);
     res.end();
   }
-
 };
+
+function handleApiResponse(movieUrl: url.Url, error: any, response: request.RequestResponse, body: string) {
+  if (!error && response.statusCode == 200) {
+    console.log(`Saving ${movieUrl.href} in cache`);
+    myCache.set<string>( movieUrl.href, body );
+
+    parseResultAndQueryNext(body);
+  } else {
+    console.warn(`Error ${body}`);
+  }
+}
+
+function parseResultAndQueryNext(body: string) {
+  const tvShowResult = <TvShowResult>JSON.parse(body);
+  const nextPage = tvShowResult.page + 1;
+  const nextPageUrl = url.parse(`${THE_MOVIE_DB_API_BASE}/discover/tv?page=${nextPage}`);
+
+  console.log(`Checking ${nextPageUrl.href} in cache`);
+  const value = myCache.get<string>( nextPageUrl.href );
+
+  if ( value == undefined ) {
+    console.log(`Pre-Fetching ${nextPageUrl.href} for future use`);
+
+    const nextPageUrlWithKey = appendQuery(nextPageUrl.href, {
+      "api_key": THE_MOVIE_DB_API_KEY
+    });
+
+    request(nextPageUrlWithKey, undefined, function (error, response: request.RequestResponse, body: string) {
+      if (!error && response.statusCode == 200) {
+          console.log(`Saving ${nextPageUrl.href} in cache`);
+
+          myCache.set<string>( nextPageUrl.href, body );
+      } else {
+          console.warn(`Error ${body}`);
+      }
+    });
+  } else {
+    console.log(`Not Pre-Fetching ${nextPageUrl.href} as it was found in cache`);
+  }
+}
